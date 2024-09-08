50b7b45ef9e1015aafbdbe4e49c92d62d62189a9
reaperhound <reaperhound69004@gmail.com>
2024-09-08 22:22:15 +0530

    almost done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import moneySvg from "/money.svg";
+import { useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [inputAmount, setInputAmount] = useState(0);
+
+  const [jars, setJars] = useState([
+    { name: "Utilities", percentage: 0.55 },
+    { name: "Long-Term", percentage: 0.15 },
+    { name: "Short Term", percentage: 0.1 },
+    { name: "Financial Freedom", percentage: 0.08 },
+    { name: "Giving", percentage: 0.05 },
+    { name: "Play", percentage: 0.07 },
+  ]);
+
+  console.log({ inputAmount });
 
   return (
     <div className="w-[100vw] h-[100vh] flex justify-center items-center">
@@ -11,15 +23,26 @@ function App() {
         {/* Input  */}
         <div className="flex gap-5">
           <label className="input input-bordered flex items-center gap-2">
-            <input type="text" className="grow" placeholder="Enter Amount" />
+            <input
+              type="number"
+              className="grow"
+              placeholder="Enter Amount"
+              value={inputAmount}
+              onChange={(val) => setInputAmount(val.target.value)}
+            />
           </label>
-          <button className="btn btn-outline btn-secondary">Split</button>
+          {/* <button className="btn btn-outline btn-secondary">Split</button> */}
         </div>
 
         {/* Values */}
-        <div className="w-[100%] grid grid-cols-3 grid-rows-2 gap-[25%] ">
-          {[1, 2, 3, 4, 5, 6].map((val, key) => (
-            <button className="btn btn-outline btn-info">{val * 1000}</button>
+        <div className="w-[100%] grid grid-cols-3 grid-rows-2 gap-[10%] ">
+          {jars.map((val, index) => (
+            <div key={index}>
+              <p>{val.name}</p>
+              <button className="btn btn-outline w-[100%] btn-info mt-2">
+                {Math.floor(+inputAmount * val.percentage)}
+              </button>
+            </div>
           ))}
         </div>
       </div>
