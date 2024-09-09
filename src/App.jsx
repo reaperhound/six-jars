95ea06c68df1853e514344805b24099c1e303507
reaperhound <reaperhound69004@gmail.com>
2024-09-09 14:31:08 +0530

    bug(typo)

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ function App() {
         <div className="flex gap-5">
           <label className="form-control w-full max-w-xs">
             <div className="label">
-              <span className="label-text">What is your name?</span>
+              <span className="label-text">Enter Amount</span>
             </div>
             <input
               type="number"
